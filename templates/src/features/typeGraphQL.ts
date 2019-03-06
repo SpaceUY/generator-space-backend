23270e9b109073b41068d6b22291cc7046047da1
Ilan Garcia <ilan.f.garcia@gmail.com>
2019-03-06 11:04:25 -0300

    Moved initialization to async

--- templates/src/features/typeGraphQL.ts
+++ templates/src/features/typeGraphQL.ts
@@ -2,6 +2,6 @@ import * as ExpressGraphQL from 'express-graphql';
 import graph from '../graph';
 import { FeatureContext } from '.';
 
-export default function ({ app }: FeatureContext) {
+export default async function ({ app }: FeatureContext) {
   app.use(ExpressGraphQL(graph));
 }
